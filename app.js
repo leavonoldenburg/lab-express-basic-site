55e25e99e08f5d632c83f581d1ac5da8f9ca5234
Lea von Oldenburg <lea92.von.oldenburg@gmail.com>
2021-08-14 16:28:21 +0200

    update with hbs

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const { response } = require('express');
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.set('views', __dirname + '/views');
 
 app.use(function (request, response, next) {
   //console.log('A request was received');
@@ -11,16 +19,29 @@ app.use(function (request, response, next) {
 
 app.use(express.static('public'));
 
-app.get('/home', function (request, response) {
-  response.sendFile(__dirname + '/views/home.html');
+app.get('/', function (request, response) {
+  //response.sendFile(__dirname + '/views/home.html');
+  response.render('home', {
+    title: 'homepage',
+    message: 'HOME',
+    article: 'Welcome to the homepage.'
+  });
 });
 
 app.get('/about', function (request, response) {
-  response.sendFile(__dirname + '/views/about.html');
+  //response.sendFile(__dirname + '/views/about.html');
+  response.render('about', {
+    title: 'about',
+    message: 'ABOUT'
+  });
 });
 
 app.get('/works', function (request, response) {
-  response.sendFile(__dirname + '/views/works.html');
+  //response.sendFile(__dirname + '/views/works.html');
+  response.render('works', {
+    title: 'works',
+    message: 'WORKS'
+  });
 });
 
 app.get('*', function (request, response) {
